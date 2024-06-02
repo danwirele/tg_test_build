90e8b7eec8bc4f95e3aae2b10c5a4fab8c3f1902
Daniil <115182119+danwirele@users.noreply.github.com>
2024-06-02 16:18:20 +0300

    fix!

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3ecbdb51dd62ba6a48809a193773c5a0",
-"/": "3ecbdb51dd62ba6a48809a193773c5a0",
+"index.html": "2b09855c9bd586f0c29c8e5c579912a3",
+"/": "2b09855c9bd586f0c29c8e5c579912a3",
 "main.dart.js": "f5f1800148c6ccb3ba3b6a18469d1968",
 "manifest.json": "1c92a17992f9dafd6a3c5b98c09fd726",
 "version.json": "0ef9baca2c0e0b8ee8a18ac38b90855d"};
